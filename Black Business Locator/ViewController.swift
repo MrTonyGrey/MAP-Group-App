54960565977e41545f89ddbc2f2f4fc943b5d9c5
Justin Stewart <justinmrstewart@gmail.com>
2021-04-06 16:33:07 -0400

    Pushing files

---
+++ Black Business Locator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Black Business Locator
+//
+//  Created by Justin Stewart on 4/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
